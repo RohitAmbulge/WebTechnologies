48a3831b0554f0f220dfa1b28fd5b493c550e2d2
Rohit Ambulge <rohitambulge15@gmail.com>
2025-12-28 23:01:30 +0530

    DOM

---
+++ JavaScript/DOM/ButtonClick.js
@@ -0,0 +1,61 @@
+// let btn = document.querySelector('button');
+// btn.addEventListener("click",()=>{
+//     alert("Button")
+// });
+
+// let p = document.querySelector("p");
+// p.addEventListener("click", ()=>{
+//     p.innerText = "Hi Im Rohit"
+// })
+
+// let btn = document.querySelector('button');
+// let h = document.querySelector("h1");
+// let count = Number(h.innerText);
+// btn.addEventListener("click",()=>{
+//     count = count+1;
+//     h.innerText = count;
+// })
+
+// let input = document.querySelector('input');
+// input.placeholder = "Enter you text"
+// input.setAttribute('id','myinp')
+// let span = document.querySelector('span');
+// input.addEventListener("input",(e)=>{
+//     console.log(e.id); 
+//     console.log(e.target.value);
+//     span.innerText = e.target.value;
+//     // span.innerText = 
+// })
+
+
+// Hover Effect
+// Change text color when mouse enters a div and revert on mouse leave.
+// let div = document.querySelector('div');
+// div.addEventListener('mousemove',()=>{
+//     div.style.backgroundColor = 'red';
+// })
+// div.addEventListener('mouseenter',function(){
+//     this.style.color = 'red';
+// })
+
+// div.addEventListener('mouseleave',function(){
+//     this.style.color = 'black';
+// })
+
+// // Show / Hide Text
+// // Button to hide and show a paragraph.
+// let btn = document.querySelector('button');
+// let p = document.querySelector('p');
+// let temp = true;
+
+// btn.addEventListener('click',()=>{
+//     if(temp){
+//         p.style.display = 'none'
+//         temp = false;
+//     }
+//     else{
+//         p.style.display = 'block'
+//         temp = true;
+//     }
+// })
+
