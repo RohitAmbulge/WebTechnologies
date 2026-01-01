3fca26fce46e8a44f8b1febf823bb76b51b5401a
Rohit Ambulge <rohitambulge15@gmail.com>
2026-01-01 13:11:37 +0530

    Event Task

--- JavaScript/DOM/ButtonClick.js
+++ JavaScript/DOM/ButtonClick.js
@@ -21,12 +21,11 @@
 // input.setAttribute('id','myinp')
 // let span = document.querySelector('span');
 // input.addEventListener("input",(e)=>{
-//     console.log(e.id); 
+//     console.log(e.id);
 //     console.log(e.target.value);
 //     span.innerText = e.target.value;
-//     // span.innerText = 
+//     // span.innerText =
 // })
-
 
 // Hover Effect
 // Change text color when mouse enters a div and revert on mouse leave.
@@ -42,8 +41,8 @@
 //     this.style.color = 'black';
 // })
 
-// // Show / Hide Text
-// // Button to hide and show a paragraph.
+//! Show / Hide Text
+//! Button to hide and show a paragraph.
 // let btn = document.querySelector('button');
 // let p = document.querySelector('p');
 // let temp = true;
@@ -59,3 +58,58 @@
 //     }
 // })
 
+// Toggle Theme
+// Button to toggle background color (light/dark).
+//1.
+// let btn = document.querySelector('button');
+// btn.addEventListener("click",()=>{
+//     document.body.classList.toggle("dark");
+// });
+
+// // 2.
+//! let btn = document.querySelector('button');
+//! if(localStorage.getItem('theme') == 'dark'){
+//     document.body.classList.add("dark");
+//     btn.innerText = "☀️";
+// }
+
+// btn.addEventListener("click",()=>{
+//     document.body.classList.toggle("dark");
+
+//     if(document.body.classList.contains("dark")){
+//         btn.innerText = "☀️";
+//         localStorage.setItem("theme","dark");
+//     }
+//     else{
+//         btn.innerText = "🌙";
+//         localStorage.setItem("theme","light");
+//     }
+// });
+
+//! Add List Item
+//! Input + button → add text as a new <li> in <ul>.
+const list = document.getElementById("list");
+const input = document.querySelector("input");
+
+const btn = document.querySelector("#inbtn");
+
+btn.addEventListener("click", () => {
+  console.log(input.value);
+  const value = input.value.trim();
+  if ( value === "") {
+    console.log("Please Enter Text");
+    return;
+  }
+
+  let item = document.createElement("li");
+  item.innerText = value;
+  item.style.background = "red";
+  list.appendChild(item);
+  input.value = ""
+});
+
+// Remove Element
+// Button to remove a paragraph from the page.
+
+// Disable Button
+// Disable a button after it is clicked once.
