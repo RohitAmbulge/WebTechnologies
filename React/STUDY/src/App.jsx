0c39eeee1d71eb0c1d51bfc3a14e204c796aac9d
Rohit Ambulge <rohitambulge15@gmail.com>
2026-01-25 00:26:05 +0530

    Mapping Task

--- React/STUDY/src/App.jsx
+++ React/STUDY/src/App.jsx
@@ -1,13 +1,15 @@
 import EventTask from "./components/eventTask/EventTask";
 import StateIntro from "./components/intro/state/StateIntro"
+import MapTask1 from "./components/MapTasks/MapTask1";
 
 let App =()=>{
     return(
         <>
         <h1>Tasks</h1>
-        <StateIntro></StateIntro>
-        <br />
-        <EventTask></EventTask>
+            {/* <StateIntro></StateIntro>
+            <br />
+            <EventTask></EventTask> */}
+            <MapTask1></MapTask1>
         </>
         
     )
